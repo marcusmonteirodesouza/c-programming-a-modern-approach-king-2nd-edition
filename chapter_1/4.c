1ff0e593419c3b9a4e49ca7cf86155a883c5a1ee
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2021-12-14 15:49:41 -0500

    Add chapter 1, 4

---
+++ chapter_1/4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "4.h"
+
+int main() {
+  double amount;
+  printf("Enter the amount in dollars: $");
+  scanf("%lf", &amount);
+  double tax_added_amount = amount + TaxValue(amount);
+  printf("The tax-added amount is: $%f", tax_added_amount);
+}
+
+double TaxValue(const double amount) {
+  return amount * 0.05;
+}
---
+++ chapter_1/4.h
@@ -0,0 +1,5 @@
+/**
+ * Returns tax value of an amount
+ * @param double The amount in dollars
+ */
+double TaxValue(const double amount);
